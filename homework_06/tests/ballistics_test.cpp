69f54521169bf13a267513b4612207c14b9288e1
serviom <oivantc@gmail.com>
2026-05-11 20:51:41 +0000

    chore: Added ctest, GTest, clang-format, cmake-format, clang-tidy. Made a skeleton for tests.

---
+++ homework_06/tests/ballistics_test.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include "ballistics.hpp"
+
+TEST(BallisticsMath, TestIsWorking) {
+    EXPECT_TRUE(true) << "тест TEST is working!"; 
+}
+
+TEST(BallisticsMath, ZeroVelocity) {
+    // EXPECT_EQ(calculateDistance(0, 45), 0);
+}
+
+TEST(BallisticsMath, ZeroVelocity2) {
+    // EXPECT_EQ(calculateDistance(0, 45), 0);
+}
+
+TEST(BallisticsMath, ZeroVelocity3) {
+    // EXPECT_EQ(calculateDistance(0, 45), 0);
+}
